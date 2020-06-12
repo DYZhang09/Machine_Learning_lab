6c59981e104f17268bc736bf13820f9c8cf26d0d
Monster Zhang <1311435497@qq.com>
2020-06-12 11:37:47 +0800

    kernel svm

--- kernel_svm.py
+++ kernel_svm.py
@@ -5,6 +5,14 @@ import cvxopt.solvers
 
 class KernelSVM(object):
     def __init__(self, kernel, C):
+        """
+        initialize kernel svm classifier.\n
+        :param kernel: Python class of kernel.
+                        the class must have a data member can be called as 'kernel.linear'
+                        (if kernel is linear kernel then kernel.linear should be True else False)
+                        and a function member can be called as 'kernel.calculate(xi, xj, *args)'
+        :param C: relaxation variable
+        """
         self.kernel = kernel
         self.C = C
         self.weight = []
@@ -15,6 +23,11 @@ class KernelSVM(object):
         self.predict_label = None
 
     def __getKernelMatrix(self, x):
+        """
+        get kernel matrix of input.\n
+        :param x: input
+        :return: kernel matrix of x
+        """
         N, _ = x.shape
         K = np.zeros((N, N))
         for i, xi in enumerate(x):
@@ -22,19 +35,26 @@ class KernelSVM(object):
                 K[i, j] = self.kernel.calculate(xi, xj)
         return K
 
-    def __getLagrangeMuil(self, input, label, C, K):
+    def __getLagrangeMulti(self, input, label, K):
+        """
+        get lagrange multiplier α.\n
+        :param input: input data with shape (N, D)
+        :param label: ground-truth label of input data with shape (N, )
+        :param K: kernel matrix of input
+        :return: lagrange multiplier α
+        """
         N = input.shape[0]
-        P = cvxopt.matrix(np.outer(label, label) * K)
+        P = cvxopt.matrix(np.outer(label, label) * K)  # get P, q, A, b for cvxopt.solvers.qp
         q = cvxopt.matrix(np.ones(N) * -1)
         A = label.reshape((1, N))
         A = A.astype('float')
         A = cvxopt.matrix(A)
         b = cvxopt.matrix(0.0)
 
-        if C is None:
+        if self.C is None:  # hard margin
             G = cvxopt.matrix(np.diag(np.ones(N) * -1))
             h = cvxopt.matrix(np.zeros(N))
-        else:
+        else:  # soft margin
             temp1 = np.diag(np.ones(N) * -1)
             temp2 = np.identity(N)
             G = cvxopt.matrix(np.vstack((temp1, temp2)))
@@ -42,19 +62,36 @@ class KernelSVM(object):
             temp2 = np.ones(N) * self.C
             h = cvxopt.matrix(np.hstack((temp1, temp2)))
 
-        result = cvxopt.solvers.qp(P, q, G, h, A, b)
+        result = cvxopt.solvers.qp(P, q, G, h, A, b)  # solve
         a = np.ravel(result['x'])
         return a
 
-    def __getBias(self, a, supprot_vec_Label, support_vec, K, indexis):
+    def __getBias(self, a, support_vec_Label, support_vec, K, indexis):
+        """
+        get bias.\n
+        :param a: lagrange multiplier α
+        :param support_vec_Label: ground-truth label of support vector
+        :param support_vec: support vector
+        :param K: kernel matrix
+        :param indexis: indexis of support vector
+        :return: bias
+        """
         b = 0
         for i in range(len(a)):
-            b += supprot_vec_Label[i]
-            b -= np.sum(a * supprot_vec_Label * K[indexis[i], support_vec])
+            b += support_vec_Label[i]
+            b -= np.sum(a * support_vec_Label * K[indexis[i], support_vec])
         b /= len(a)
         return b
 
     def __getWeight(self, features, a, support_vec, support_vec_label):
+        """
+        get weight(if kernel is linear kernel).\n
+        :param features: dimension of features
+        :param a: lagrange multiplier α
+        :param support_vec: support vector
+        :param support_vec_label: ground-truth label of support vector
+        :return: if kernel is linear kernel then return weight else return None
+        """
         if self.kernel.linear is True:
             w = np.zeros(features)
             for i in range(len(a)):
@@ -64,6 +101,12 @@ class KernelSVM(object):
         return w
 
     def __transformLabel(self, label, back=False):
+        """
+        transform labels. label[label == 0] = -1 or label[label == -1] = 0.\n
+        :param label: origin labels
+        :param back: if back is True then label[label == -1] = 0. default False
+        :return: transformed labels
+        """
         _label = label.copy()
         if back is False:
             _label[_label == 0] = -1
@@ -72,11 +115,17 @@ class KernelSVM(object):
         return _label
 
     def train(self, train_data, train_label):
+        """
+        train for kernel svm classifier.\n
+        :param train_data: data for training with shape (N, D)
+        :param train_label: ground-truth label of train data with shape (N, )
+        :return: None
+        """
         label = self.__transformLabel(train_label)
 
         N, D = train_data.shape
         K = self.__getKernelMatrix(train_data)
-        a = self.__getLagrangeMuil(train_data, label, self.C, K)
+        a = self.__getLagrangeMulti(train_data, label, K)
         support_vec = a > 1e-5
         indexis = np.arange(len(a))[support_vec]
         self.a = a[support_vec]
@@ -87,6 +136,11 @@ class KernelSVM(object):
         self.weight = self.__getWeight(D, self.a, self.support_vec, self.support_vec_label)
 
     def predict(self, test_data):
+        """
+        predict labels of data.\n
+        :param test_data: data that needs to predict with shape(N, D)
+        :return: predict labels
+        """
         if self.weight is not None:
             label = np.sign(test_data @ self.weight + self.bias)
             self.predict_label = self.__transformLabel(label, True)
@@ -95,13 +149,18 @@ class KernelSVM(object):
             for i in range(len(test_data)):
                 s = 0
                 for a, sv_y, sv in zip(self.a, self.support_vec_label, self.support_vec):
-                    s += a * sv_y * self.kernel.calculate(test_data[i], sv)
+                    s += a * sv_y * self.kernel.calculate(test_data[i], sv)  # model of kernel svm f(x)
                 predict_label[i] = s
             label = np.sign(predict_label + self.bias)
             self.predict_label = self.__transformLabel(label, True)
         return self.predict_label.copy()
 
     def calcAccuracy(self, test_label):
+        """
+        calculate accuracy of predict labels.\n
+        :param test_label: ground-truth label of test data with shape (N, )
+        :return: accuracy
+        """
         diff = test_label - self.predict_label
         corr_num = diff[diff == 0].size
         return corr_num / test_label.shape[0]
@@ -109,8 +168,14 @@ class KernelSVM(object):
 
 (train_data, train_label), (test_data, test_label) = getData(r"H:\机器学习\结课实验\income.csv",
                                                              keep_dim=False)
-print(train_data.shape, test_data.shape)
-svm = KernelSVM(LinearKernel(), 1)
-svm.train(train_data, train_label)
-label = svm.predict(test_data)
-print(svm.calcAccuracy(test_label))
+sigmas = [1, 3, 5, 7, 9, 11, 13]
+accuracies = []
+for sigma in sigmas:
+    svm = KernelSVM(GaussianKernel(sigma=sigma), 1)
+    svm.train(train_data, train_label)
+    label = svm.predict(test_data)
+    accuracies.append(svm.calcAccuracy(test_label))
+    print(accuracies[-1])
+plt.plot(sigmas, accuracies)
+plt.grid()
+plt.show()
